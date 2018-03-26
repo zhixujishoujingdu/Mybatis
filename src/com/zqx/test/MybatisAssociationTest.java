24a696d3c260b7b79797028f0d526721c076f3ad
zqx <https://github.com/zhixujishoujingdu/Mybatis.git>
2018-03-26 21:07:34 +0800

    mybatis 关联映射1对多

--- src/com/zqx/test/MybatisAssociationTest.java
+++ src/com/zqx/test/MybatisAssociationTest.java
@@ -1,19 +1,39 @@
 package com.zqx.test;
 
 import com.zqx.po.Person;
+import com.zqx.po.User;
 import com.zqx.util.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 public class MybatisAssociationTest {
+
+    /**
+     * 一对一
+     */
     @Test
     public void findPersonById(){
         SqlSession sqlSession= MybatisUtil.getSession();
         Person person=sqlSession.selectOne(
-                "com.zqx.mapper"+".PersonMapper.findPersonById",1);
+                "com.zqx.mapper.PersonMapper.findPersonById",1);
 
         System.out.println(person);
         sqlSession.commit();
         sqlSession.close();
     }
+
+    /**
+     * 一对多
+     */
+    @Test
+    public void findUserTest(){
+        SqlSession sqlSession = MybatisUtil.getSession();
+        User user =sqlSession.selectOne(
+                "com.zqx.mapper"+".UserMapper.findUserByOrders",1);
+        System.out.println(user);
+        sqlSession.close();
+    }
+
+
+
 }
