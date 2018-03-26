e81461959e5341cee43104a019289045bf4c3eb5
zqx <https://github.com/zhixujishoujingdu/Mybatis.git>
2018-03-26 22:37:34 +0800

    mybatis 关联映射多对多

--- src/com/zqx/test/MybatisAssociationTest.java
+++ src/com/zqx/test/MybatisAssociationTest.java
@@ -1,10 +1,12 @@
 package com.zqx.test;
 
+import com.zqx.po.Orders;
 import com.zqx.po.Person;
 import com.zqx.po.User;
 import com.zqx.util.MybatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+import org.springframework.core.annotation.Order;
 
 public class MybatisAssociationTest {
 
@@ -31,6 +33,20 @@ public class MybatisAssociationTest {
         User user =sqlSession.selectOne(
                 "com.zqx.mapper"+".UserMapper.findUserWithOrders",1);
         System.out.println(user);
+        sqlSession.close();
+    }
+
+    /**
+     * 多对多
+     */
+    @Test
+    public void findOrdersWithProductTest(){
+
+        SqlSession sqlSession = MybatisUtil.getSession();
+        Orders orders =sqlSession.selectOne(
+                "com.zqx.mapper"+
+                        ".OrdersMapper.findOrdersWithProduct",1);
+        System.out.println(orders);
         sqlSession.close();
     }
 
